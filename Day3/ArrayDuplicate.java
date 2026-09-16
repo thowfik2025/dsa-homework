7b441ce4d2d6e6cbbbcf904f197f26134640f996
thowfik2025 <thowfikthowfik32@gmail.com>
2026-09-16 09:13:06 +0530

    day3 work

---
+++ Day3/ArrayDuplicate.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class ArrayDuplicate {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (int x : arr) {
+
+            if (set.contains(x)) {
+                result.add(x);
+            } else {
+                set.add(x);
+            }
+        }
+
+        if (result.isEmpty()) {
+            System.out.println("No duplicates found");
+        } else {
+            System.out.println("Duplicate elements: " + result);
+        }
+    }
+}
