fd21fb37ac64a132f12010db3e73e97deffbd40a
thowfik2025 <thowfikthowfik32@gmail.com>
2026-09-18 10:37:13 +0530

    unique sum user defined

---
+++ UserDefindMethods/SumuniqueArray.java
@@ -0,0 +1,26 @@
+package UserDefindMethods;
+import java.util.*;
+public class SumuniqueArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+            }
+            if(count==1){
+                sum+=arr[i];
+            }
+        }
+        System.out.println(sum);
+
+    }
+}
